bc57066f246465ff1c485c594c43a0c03ae96826
LucaB98 <lucabellan98@gmail.com>
2023-12-22 17:24:25 +0100

    print in the page result

--- js/script.js
+++ js/script.js
@@ -10,6 +10,22 @@ console.log('JS OK')
 
 const user = document.querySelector('.user');
 const pc = document.querySelector('.pc');
-const winner = document.getElementById(winner);
-const risult = document.getElementById(risult);
+const winner = document.getElementById('winner');
+const risult = document.getElementById('risult');
 
+const userNumber = Math.floor(Math.random()*6) + 1;
+const pcNumber = Math.floor(Math.random()*6) + 1;
+
+console.log(userNumber, pcNumber)
+
+let message = 'pareggio';
+
+if(userNumber > pcNumber){
+    message = "Ha vinto l'utente";
+}else if(userNumber < pcNumber){
+    message = 'Ha vinto il pc';
+}
+user.innerText = userNumber;
+pc.innerText = pcNumber;
+
+winner.innerText = message;
