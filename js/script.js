a937f02c361bf5fea70ff3b73fe24b5329c7e72f
LucaB98 <lucabellan98@gmail.com>
2023-12-22 19:01:15 +0100

    added validation

--- js/script.js
+++ js/script.js
@@ -6,7 +6,37 @@ console.log('JS OK')
 // 4 - controllo dell'email inserita nel form
 // 5 - validazione della email
 
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+const error = document.querySelector('.invalid-feedback')
 
+console.log(input, button, form, result)
 
+const validEmail = ['luca@bellan.it', 'matteo@rogato.it', 'giulia@tagliareni.it'];
 
+console.log(validEmail)
 
+button.addEventListener('click', function(){
+    const userEmail = input.value.trim();
+    console.log(userEmail);
+
+    if (!userEmail){
+        input.classList.add('is-invalid');
+        return;
+    };
+
+    let userIsAllowed = false;
+
+    for(let i = 0; i < validEmail.length; i++){
+        console.log('mail controllata: ', validEmail[i]);
+        console.log('mail dell utente: ', userEmail);
+
+        if(userEmail === validEmail[i]){
+            console.log('trovata');
+        }
+    }
+});
+
+ 
