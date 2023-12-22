aa2d74754678c709033b81f7b8548146ab5803d1
LucaB98 <lucabellan98@gmail.com>
2023-12-22 17:11:44 +0100

    element and scale js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('JS OK')
+
+// 1 - prendere gli elementi interessati
+// 2 - generare un numero random sia per pc che utente
+// 3 - capire chi fa punteggio piu alto e assegnarlo vincitore
+// 4 - stampare in pagina tiri e risultato
+
+
+
+
+const user = document.querySelector('.user');
+const pc = document.querySelector('.pc');
+const winner = document.getElementById(winner);
+const risult = document.getElementById(risult);
+
